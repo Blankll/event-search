baa2c7ba66e8bb9c6b00d95ff6ca07a9037cd951
seven <zilisheng1996@gmail.com>
2023-07-11 00:39:46 +0800

    integrate lint

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,19 @@
-import express, {Request, Response} from "express";
-import cors from "cors";
-
+import express, { Request, Response } from 'express';
+import cors from 'cors';
 
 // create and setup express index
 const index = express();
 index.use(express.json());
-index.use(cors({origin: true, credentials: true}))
-
+index.use(cors({ origin: true, credentials: true }));
 
 index.get('/api/cookie/tst', (eq: Request, res: Response) => {
-  res.json({message: `success ${eq.method}`});
-})
+  res.json({ message: `success ${eq.method}` });
+});
 index.post('/api/cookie/tst', (eq: Request, res: Response) => {
-  res.json({message: `success ${eq.method}`});
-})
+  res.json({ message: `success ${eq.method}` });
+});
 
 // start express server
 index.listen(4000, () => {
-  console.log('service start at 4000')
+  console.log('service start at 4000');
 });
